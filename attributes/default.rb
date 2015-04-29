aaf180d7bdd08ef64cae82e38268628ca40fd4de
Zeppelin <zpallin@gmail.com>
2015-04-28 20:45:15 -0700

    Update default.rb
    debug mode = false by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,4 @@ node.default[:cyclesafe_chef][:directory] = '/srv/cyclesafe'
 node.default[:cyclesafe_chef][:requirements_file] = 'prod_requirements.txt'
 node.default[:cyclesafe_chef][:db_name] = 'default'
 node.default[:cyclesafe_chef][:hostname] = 'cyclesafe.us'
+node.default[:cyclesafe_chef][:debug_mode] = false
